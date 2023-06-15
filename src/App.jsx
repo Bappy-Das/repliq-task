4ff805ab9d6578aeed4e2f7197793c1abe86745b
Bappy dase <bappydas.bd51@gmail.com>
2023-06-15 18:46:54 +0600

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-blue-500 text-white">
+      <h1 className="text-2xl">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
